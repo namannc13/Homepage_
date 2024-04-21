49641faf118f83777234ce0d5e7b240b7ff8b14c
namannc13 <chawhannaman5@gmail.com>
2024-04-22 04:00:59 +0530

    modified cors configurations

--- server/index.js
+++ server/index.js
@@ -2,17 +2,17 @@ import express from "express";
 import "./config/db.js";
 import { authRouter } from "./routes/auth.route.js";
 import { linkRouter } from "./routes/link.route.js";
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
-app.use(cors(
-  {
-    origin: ["http://localhost:5173"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
