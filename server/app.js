b2c3d11059ef15bf161aff535d6ab78fc5128c87
namannc13 <chawhannaman5@gmail.com>
2024-04-22 19:40:17 +0530

    bug fix

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://homepage-full-stack-thvn.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   })
